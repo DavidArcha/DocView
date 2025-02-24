9d16957e623c92d035d4195a4609c43b63ed18dc
DavidArcha <davidarcha8@gmail.com>
2025-02-24 11:44:01 +0530

    Refactor accordion data structure for improved consistency; introduce unique IDs for items; enhance search group fields with structured titles and IDs; update styles for better visual consistency in SimpleSearchComponent.

--- src/app/Shared/Common/search_groupfields.ts
+++ src/app/Shared/Common/search_groupfields.ts
@@ -1,24 +1,24 @@
 export const searchGroupFields = [
     {
-        groupTitle: "Saved Groups",
+        groupTitle: { id: 'saved_group', title: "Saved Groups" },
         groupFields: [
             {
-                "title": "Copy of Testing",
-                "fields": [{ parent: "System Fields", field: "Copy", operator: "yes", value: "" },
-                { parent: "System Fields", field: "Date", operator: "equals", value: "11-02-2025" },
+                "title": { id: 'copy_of_testing', title: "Copy of Testing" },
+                "fields": [{ id: 'copy', parent: "System Fields", field: "Copy", operator: "yes", value: "" },
+                { id: 'date', parent: "System Fields", field: "Date", operator: "equals", value: "11-02-2025" },
                 ]
             },
             {
-                "title": "Testing345",
-                "fields": [{ parent: "System Fields", field: "Name", operator: "contains", value: "" },
-                { parent: "System Fields", field: "Age", operator: "empty", value: "active" },
-                { parent: "System Fields", field: "Date", operator: "End-On", value: "2025-02-11" },
+                "title": { id: 'testing345', title: "Testing345" },
+                "fields": [{ id: 'name', parent: "System Fields", field: "Name", operator: "contains", value: "" },
+                { id: 'age', parent: "System Fields", field: "Age", operator: "empty", value: "active" },
+                { id: "date", parent: "System Fields", field: "Date", operator: "End-On", value: "2025-02-11" },
                 ],
             },
             {
-                "title": "Testing678",
-                "fields": [{ parent: "System Fields", field: "Name", operator: "contains", value: "" },
-                { parent: "System Fields", field: "Age", operator: "equals", value: "active" },
+                "title": { id: 'testing678', title: "Testing678" },
+                "fields": [{ id: 'name', parent: "System Fields", field: "Name", operator: "contains", value: "" },
+                { id: 'age', parent: "System Fields", field: "Age", operator: "equals", value: "active" },
                 ],
             }
         ]
